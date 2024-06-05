9b3cb5dc4465f4cd4de6a0bd4a0a09b8a84675ae
Rafi Maul <rafi.maul30@gmail.com>
2024-06-05 17:48:36 +0700

    5/6/2024

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        customRed: '#a40000',  // Warna yang ingin Anda tambahkan
+      },
+    },
+  },
+  plugins: [],
+}
+
